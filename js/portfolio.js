727c4a8775e3f693a23990c69e650a1f6f82ce1a
Gokulgk001 <gokulkanthgk@gmail.com>
2024-08-21 00:03:24 +0530

    added new things

--- js/portfolio.js
+++ js/portfolio.js
@@ -99,3 +99,39 @@ window.addEventListener("scroll", () => {
     })
 })
 
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click",() => {
+  navigation.classList.add("active");
+} );
+
+closeBtn.addEventListener("click",() => {
+  navigation.classList.remove("active");
+} );
+
+navItems.forEach((navItems) => {
+  navItems.addEventListener("click",() => {
+    navigation.classList.remove("active");
+  });
+});
+
+ScrollReveal({
+  reset: true,
+  distance:'60px',
+  duration: 2500 ,
+  delay:100
+});
+
+ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02 ', {delay:500, origin:'left'});
+ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', {delay:600, origin:'right'});
+ScrollReveal().reveal('.home .info .btn', {delay:700, origin:'bottom'});
+ScrollReveal().reveal('.media-icons i, .contact-left li', {delay:500, origin:'left', interval:200});
+ScrollReveal().reveal('.home-img, .about-img', {delay:500, origin:'bottom'});
+ScrollReveal().reveal('.about .description, .contact-right', {delay:600, origin:'right'});
+ScrollReveal().reveal('.about .tab-titles,.tab-contents', {delay:500, origin:'right', interval:200});
+ScrollReveal().reveal('.skills-description, .skills-info,.contact-card ,.contact-left h2', {delay:700, origin:'left', interval:200});
+ScrollReveal().reveal('.portfolio .img-card', {delay:800, origin:'bottom',interval:200});
+ScrollReveal().reveal('footer .group', {delay:500, origin:'top',interval:200});
